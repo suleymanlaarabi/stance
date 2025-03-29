fa19cfef11f27b1a64b1dd2fdf009b6915ab2aca
Suleyman Laarabi <suleyman.laarabi@epitech.eu>
2025-03-29 23:37:18 +0100

    export gravix and component

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,15 @@
 use bevy::prelude::*;
 
 pub mod components;
-pub mod plugins;
 pub mod prelude;
 mod systems;
 
-pub use plugins::grounded_plugin::components::*;
 use systems::{camera::rotate_player_camera, jump::handle_jump, movement::move_player};
 
 pub struct VeloxCharacterPlugin;
 
 impl Plugin for VeloxCharacterPlugin {
     fn build(&self, app: &mut App) {
-        // app.add_plugins(CharacterGroundedPlugin);
         app.add_systems(Update, (move_player, rotate_player_camera, handle_jump));
     }
 }
---
+++ src/prelude.rs
@@ -0,0 +1,2 @@
+pub use crate::components::*;
+pub use gravix::components::*;
